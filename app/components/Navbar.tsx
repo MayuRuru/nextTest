37e7647e3e21b06512745969334fd41a630d73c3
MayuRuru <aruizrull@protonmail.com>
2023-03-26 20:39:24 +0200

    First commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="">
+      <h1>
+        <Link href="/">MayuNext</Link>
+      </h1>
+    </nav>
+  );
+}
