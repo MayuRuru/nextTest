75f65660234bf828123c8c8bd14c6247fe7e235c
MayuRuru <aruizrull@protonmail.com>
2023-03-27 09:22:36 +0200

    Display results in searchPage and add metada handling notfound

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import getResults from "@/lib/getResults";
 
 type Props = {
   params: {
@@ -6,6 +7,40 @@ type Props = {
   };
 };
 
-export default function page({ params: { searchTerm } }: Props) {
-  return <div>page</div>;
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getResults(searchTerm);
+  const data = await wikiData;
+  const displayTerm = searchTerm.replaceAll("%20", " ");
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${displayTerm} Not Found`,
+    };
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search resulsts fro ${displayTerm}`,
+  };
+}
+
+export default async function SearchResults({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getResults(searchTerm);
+  const data = await wikiData;
+  const results: Result[] | undefined = data?.query?.pages;
+
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+      {results ? (
+        Object.values(results).map((result) => {
+          // eslint-disable-next-line react/jsx-key
+          return <p>{JSON.stringify(result)}</p>;
+        })
+      ) : (
+        <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
+      )}
+    </main>
+  );
+
+  return content;
 }
